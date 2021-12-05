4801916c7c120d4fa7d0f316baf442cc63b8a53c
Daniela Brailovsky <dbrailo@gmail.com>
2021-12-05 17:41:07 +0200

    updated delete option

--- app/routes.py
+++ app/routes.py
@@ -148,7 +148,7 @@ def delete_post(post_id):
     post = Post.query.get_or_404(post_id)
     if post.user != current_user:
         abort(403)
-        db.session.delete(post_id)
-        db.session.commit()
-        flash('Your post has been deleted', 'success')
-        return redirect(url_for('main'))
+    db.session.delete(post)
+    db.session.commit()
+    flash('Your post has been deleted', 'success')
+    return redirect(url_for('main'))
